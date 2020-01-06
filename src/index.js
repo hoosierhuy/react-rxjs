3cdaaad875a25c84906a9a752ecf14438259b84d
Huy Nguyen <huynguyen@Huys-MacBook-Pro.local>
2020-01-06 11:35:04 -0500

    First commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import { map } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const apiEndPoint = `https://randomuser.me/api/?results=5&nat=us`;
+const getUserPic = user => `${user.picture.medium}`;
+
+const pics$ = ajax
+  .getJSON(apiEndPoint)
+  .pipe(map(({ results: users }) => users.map(getUserPic)));
+
+// React's Hooks and Effects Utility Function to keep things DRY if
+// you were to have multiple observables.
+const useObservable = observable => {
+  const [state, setState] = useState();
+
+  useEffect(() => {
+    const sub = observable.subscribe(setState);
+
+    return () => sub.unsubscribe();
+  }, [observable]);
+
+  return state;
+};
+
+const PicComponent = ({ pics = [], loading = false }) => (
+  <div className={loading ? 'loading' : null}>
+    {pics.map(pic => (
+      <section key={pic}>
+        <img src={pic} alt="user's photograph" />
+      </section>
+    ))}
+  </div>
+);
+
+function App() {
+  const pics = useObservable(pics$);
+
+  return (
+    <div className="App">
+      <h1>React and Basic RxJS</h1>
+
+      <PicComponent pics={pics} />
+    </div>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
